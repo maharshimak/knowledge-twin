52b1b9e37479ae69c984be8772b42a9a5459fe1e
Maharshi Patel <pmaharshi999@gmail.com>
2026-09-23 10:55:45 +0200

    feat(graph): export canonical entity-resolution API

--- src/knowledge_twin/__init__.py
+++ src/knowledge_twin/__init__.py
@@ -1,4 +1,14 @@
 from .graph import Edge, Entity, KnowledgeGraph
+from .resolution import ResolutionMatch, entity_similarity, normalize_entity_name, resolve_entity
 from .storage import SQLiteGraphStore
 
-__all__ = ["Edge", "Entity", "KnowledgeGraph", "SQLiteGraphStore"]
+__all__ = [
+    "Edge",
+    "Entity",
+    "KnowledgeGraph",
+    "ResolutionMatch",
+    "SQLiteGraphStore",
+    "entity_similarity",
+    "normalize_entity_name",
+    "resolve_entity",
+]
